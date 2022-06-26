4820752c3f22e48732fca79c783becd19eb7907f
Anthony Henry <anthonysjhenry@gmail.com>
2022-06-26 15:43:02 -0400

    more flask building

--- Web_Dev_Practice/flask_demo.py
+++ Web_Dev_Practice/flask_demo.py
@@ -3,9 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ah:ah@localhost:5432/ah' #<dialect>://user:pass@<host>:<port>/<dbname>
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Person(db.Model):
+    # __tablename__ = 'people' <-- if you want to change tablename
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
 
+db.create_all()
 
 @app.route('/')
 def index():
